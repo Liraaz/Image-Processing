910c8a5629a1dfceeda2c1a51af1b9c67fdd5b9d
Himadrijr10 <145125968+Himadrijr10@users.noreply.github.com>
2023-09-16 23:16:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Face_Recognition_System:
         self.photoimg2=ImageTk.PhotoImage(img2)
 
         f_lbl=Label(self.root,image=self.photoimg2)
-        f_lbl.place(x=530,y=0,width=535,height=130)
+        f_lbl.place(x=535,y=0,width=535,height=130)
 
         # 3rd image
         img3=Image.open(r"C:\Users\HIMADRI\Desktop\Project\Images\College.jpg")#insert a image
@@ -31,17 +31,28 @@ class Face_Recognition_System:
         self.photoimg3=ImageTk.PhotoImage(img3)
 
         f_lbl=Label(self.root,image=self.photoimg3)
-        f_lbl.place(x=1060,y=0,width=535,height=130)
+        f_lbl.place(x=1070,y=0,width=535,height=130)
 
 
         # bg image
         img4=Image.open(r"C:\Users\HIMADRI\Desktop\Project\Images\College.jpg")#insert a image
-        img4=img4.resize((530,130))#resize((530,130),Image.ANTIALIAS)
+        img4=img4.resize((1600,900))#resize((530,130),Image.ANTIALIAS)
         self.photoimg4=ImageTk.PhotoImage(img4)
 
-        bg_lbl=Label(self.root,image=self.photoimg4)
-        bg_lbl.place(x=0,y=130,width=535,height=130)
+        bg_img=Label(self.root,image=self.photoimg4)
+        bg_img.place(x=0,y=130,width=1600,height=700)
 
+
+        title_lbl=Label(bg_img,text="FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE",font=("times new roman",36,"bold"),bg="white",fg="blue")
+        title_lbl.place(x=0,y=0,width=1600,height=45)
+
+        # student button
+        img5=Image.open(r"C:\Users\HIMADRI\Desktop\Project\Images\College.jpg")#insert a image
+        img5=img5.resize((1600,900))#resize((530,130),Image.ANTIALIAS)
+        self.photoimg5=ImageTk.PhotoImage(img5)
+
+        b1=Button(bg_img,image=self.photoimg5)
+        b1.place(x=200,y=100,width=180,height=180)
 
 
 
