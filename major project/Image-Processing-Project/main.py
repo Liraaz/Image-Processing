bfe7f5f4612d94366a121b90ef3249268ee41e6d
Liraaz <143328050+Liraaz@users.noreply.github.com>
2023-09-22 00:13:20 +0530

    Add files via upload

---
+++ major project/Image-Processing-Project/main.py
@@ -0,0 +1,55 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+
+
+class Face_Recognition_System:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1920x1080+0+0")#you can set your own monitor resolution
+        self.root.title("Face Recognition System")
+
+        # 1st image
+        img1=Image.open(r"C:\Users\HIMADRI\Desktop\Project\Images\College.jpg")#insert a image
+        img1=img1.resize((530,130))#resize((530,130),Image.ANTIALIAS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=0,y=0,width=535,height=130)
+
+        # 2nd image
+        img2=Image.open(r"C:\Users\HIMADRI\Desktop\Project\Images\College.jpg")#insert a image
+        img2=img2.resize((530,130))#resize((530,130),Image.ANTIALIAS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=530,y=0,width=535,height=130)
+
+        # 3rd image
+        img3=Image.open(r"C:\Users\HIMADRI\Desktop\Project\Images\College.jpg")#insert a image
+        img3=img3.resize((530,130))#resize((530,130),Image.ANTIALIAS)
+        self.photoimg3=ImageTk.PhotoImage(img3)
+
+        f_lbl=Label(self.root,image=self.photoimg3)
+        f_lbl.place(x=1060,y=0,width=535,height=130)
+
+
+        # bg image
+        img4=Image.open(r"C:\Users\HIMADRI\Desktop\Project\Images\College.jpg")#insert a image
+        img4=img4.resize((530,130))#resize((530,130),Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        bg_lbl=Label(self.root,image=self.photoimg4)
+        bg_lbl.place(x=0,y=130,width=535,height=130)
+
+
+
+
+
+
+if __name__=="__main__":
+    root=Tk()
+    obj=Face_Recognition_System(root)
+    root.mainloop()
+
+
